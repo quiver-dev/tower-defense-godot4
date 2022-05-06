616f766826e949085cd8eb82ceef9999df19be22
dodoveloper <davide.cor94@gmail.com>
2022-05-06 17:38:53 +0200

    Laid out bullet scene and script (WIP)

---
+++ entities/bullets/bullet.gd
@@ -0,0 +1,22 @@
+extends Area2D
+# This class extends the Area2D node because we don't need advanced
+# physics or player-controlled movement.
+# It also uses a VisibleOnScreenEnabler2D node to automatically
+# disable this scene when the bullet exits the screen.
+# WARN: this is probably not optimal due to the presence of a camera
+
+
+@export var speed: int = 1000
+@export var damage: int = 10
+
+var velocity: Vector2
+
+
+func _on_bullet_body_entered(body: Node2D) -> void:
+	if body is Enemy:
+		# TODO: trigger hit state for enemy
+		queue_free()
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	queue_free()
