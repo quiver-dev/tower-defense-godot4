b27da00a46beed10313f850229a8d5212593c3c5
dodoveloper <davide.cor94@gmail.com>
2022-05-25 19:47:31 +0200

    Coded a basic enemy spawner (WIP)

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -4,9 +4,8 @@ extends Node2D
 @onready var objective: Area2D = $Objective
 @onready var tile_map: TileMap = $TileMap
 @onready var camera: Camera2D = $Camera2D
-@onready var enemies: Node2D = $Enemies
+@onready var spawner: Spawner = $Spawner
 
 
 func _ready() -> void:
-	for enemy in enemies.get_children():
-		(enemy as Enemy).move_to(objective.global_position)
+	spawner.start(objective.global_position)
---
+++ scenes/main/spawner.gd
@@ -0,0 +1,35 @@
+class_name Spawner
+extends Node2D
+
+
+@export_range(0.5, 5.0, 0.5) var spawn_rate: float = 2.0
+@export var enemy: PackedScene
+@export var enemy_count: int = 10
+
+var objective_pos: Vector2
+
+@onready var spawn_timer: Timer = $SpawnTimer
+@onready var enemies: Node2D = $Enemies
+
+
+# Called once by parent scene when ready
+func start(_objective_pos: Vector2) -> void:
+	objective_pos = _objective_pos
+	_start_timer()
+
+
+func _start_timer() -> void:
+	spawn_timer.start(randf_range(spawn_rate / 2, spawn_rate))
+
+
+func _on_spawn_timer_timeout() -> void:
+	# spawn an enemy
+	var _enemy: Enemy = enemy.instantiate()
+	enemies.add_child(_enemy, true)
+	_enemy.move_to(objective_pos)
+	# reduce enemy count and repeat if necessary
+	enemy_count -= 1
+	if enemy_count > 0:
+		_start_timer()
+	else:
+		spawn_timer.stop()
