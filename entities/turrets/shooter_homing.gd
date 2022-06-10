c321250ab8e8de4a6fb114e96251dd83f5ceabf5
dodoveloper <davide.cor94@gmail.com>
2022-06-10 16:47:32 +0200

    Made level 3 turret shoot one missile at a time

--- entities/turrets/shooter_homing.gd
+++ entities/turrets/shooter_homing.gd
@@ -2,6 +2,9 @@
 extends Shooter
 
 
+var muzzle_idx := 0  # used to determine which missile to fire
+
+
 func _ready() -> void:
 	super()
 	# spawn the wanted bullets
@@ -10,24 +13,30 @@ func _ready() -> void:
 
 func shoot() -> void:
 	can_shoot = false
-	# move children to the bullet container to prevent the gun's rotation
-	# from influencing the missiles' rotation
-	for muzzle in gun.get_children():
-		var bullet: Bullet = muzzle.get_child(0)
-		muzzle.remove_child(bullet)
-		bullet_container.add_child(bullet, true)
-		bullet.start(muzzle.global_position,
-				gun.rotation + randf_range(-bullet_spread, bullet_spread),
-				targets.front())
+	# move children to the bullet container to prevent the gun's 
+	# rotation from influencing the missiles' rotation
+	var muzzle = gun.get_child(muzzle_idx)
+	var bullet: Bullet = muzzle.get_child(0)
+	muzzle.remove_child(bullet)
+	bullet_container.add_child(bullet, true)
+	bullet.start(muzzle.global_position,
+			gun.rotation + randf_range(-bullet_spread, bullet_spread),
+			targets.front())
 	firerate_timer.start(fire_rate)
+	muzzle_idx = _wrap_index(muzzle_idx + 1, bullet_count)
 	# show reload time on HUD
 	emit_signal("has_shot", firerate_timer.wait_time)
 
 
+# Function to wrap an index around an array (circular array)
+func _wrap_index(index: int, size: int) -> int:
+	return ((index % size) + size) % size
+
+
 func _spawn_missiles() -> void:
 	for i in gun.get_child_count():
-		if bullet_type:
-			var muzzle := gun.get_child(i)
+		var muzzle := gun.get_child(i)
+		if bullet_type and muzzle.get_child_count() == 0:
 			var bullet: Bullet = bullet_type.instantiate()
 			bullet.name = "Missile%d" % (i + 1)
 			bullet.rotation = -PI / 2
