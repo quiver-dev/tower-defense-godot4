5f989f027a7e04552f65ab67a10af8c17130207b
dodoveloper <davide.cor94@gmail.com>
2022-07-22 19:27:52 +0200

    Fixed tank's gun not playing the 'explode' animation when destroyed

---
+++ entities/enemies/tanks/states/die.gd
@@ -0,0 +1,9 @@
+extends "res://entities/enemies/states/die.gd"
+
+
+# Plays the "explode" animation for the shooter module.
+# Note that it MUST have the same FPS and number of frames of the
+# tank basement's "explode" animation.
+func enter() -> void:
+	super.enter()
+	(owner as Tank).shooter.explode()
