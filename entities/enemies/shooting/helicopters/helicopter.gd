7bce0bbb7adb6beb719bf62db0fa58d11b1038d8
dodoveloper <davide.cor94@gmail.com>
2022-09-14 20:01:17 +0200

    Added all helicopter's animations

--- entities/enemies/shooting/helicopters/helicopter.gd
+++ entities/enemies/shooting/helicopters/helicopter.gd
@@ -6,10 +6,23 @@ extends ShootingEnemy
 @onready var rotor := $Rotor as AnimatedSprite2D
 
 
+func _ready() -> void:
+	super()
+	# make sure all animated sprites are synchronized
+	for animated_sprite in [sprite, shadow, shooter.gun]:
+		(animated_sprite as AnimatedSprite2D).frame = 0
+		(animated_sprite as AnimatedSprite2D).playing = true
+
+
 func _physics_process(delta: float) -> void:
 	super(delta)
-	# Rotate based on current velocity
-	sprite.global_rotation = shooter.rotation
+	# handle all sprites' rotation
+	if shooter.targets.is_empty():
+		sprite.global_rotation = _calculate_rot(sprite.global_rotation,
+			velocity.angle(), rot_speed, delta)
+		shooter.rotation = sprite.global_rotation
+	else:
+		sprite.global_rotation = shooter.rotation
 	shadow.global_rotation = sprite.global_rotation
 	rotor.global_rotation = sprite.global_rotation
 
