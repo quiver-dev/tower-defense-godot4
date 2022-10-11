7899d5302e4293d85a12f5ab5c060f833b23f3b3
dodoveloper <davide.cor94@gmail.com>
2022-10-11 19:35:44 +0200

    Fixed Helicopter's transition into shoot_still state

--- entities/enemies/shooting/helicopters/states/move.gd
+++ entities/enemies/shooting/helicopters/states/move.gd
@@ -1,13 +1,8 @@
-extends "res://entities/enemies/shooting/states/move.gd"
+extends "res://entities/enemies/states/move.gd"
 
 
-func _on_detector_body_entered(body: Node2D) -> void:
-	if not body in (owner as ShootingEnemy).shooter.targets:
-		(owner as ShootingEnemy).shooter.targets.append(body)
-		finished.emit("shoot_still")
-
-
-func _on_detector_area_entered(area: Area2D) -> void:
-	if not area in (owner as ShootingEnemy).shooter.targets:
-		(owner as ShootingEnemy).shooter.targets.append(area)
+func update(delta: float) -> void:
+	super(delta)
+	# check if we need to exit the state
+	if (owner as ShootingEnemy).shooter.targets.size() > 0:
 		finished.emit("shoot_still")
--- entities/enemies/shooting/helicopters/states/shoot.gd
+++ entities/enemies/shooting/helicopters/states/shoot.gd
@@ -1,8 +1,18 @@
 extends "res://entities/enemies/shooting/states/shoot.gd"
 
 
-# Override to make helicopter stop when shooting
 func update(_delta: float) -> void:
 	if (owner as ShootingEnemy).shooter.can_shoot and \
 			(owner as ShootingEnemy).shooter.lookahead.is_colliding():
 		(owner as ShootingEnemy).shooter.shoot()
+		var shooting_anim := (owner as ShootingEnemy).shooter.gun.animation
+		(owner as ShootingEnemy).apply_animation(shooting_anim)
+	if (owner as ShootingEnemy).shooter.targets.is_empty():
+		finished.emit("move")
+
+
+func on_animation_finished() -> void:
+	# restore the move animation when shoot_a/b are finished
+	var cur_anim := String((owner as ShootingEnemy).sprite.animation)
+	if cur_anim in ["shoot_a", "shoot_b"]:
+		(owner as ShootingEnemy).apply_animation("move")
